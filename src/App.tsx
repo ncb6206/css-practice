efbb512217bd5f999d44dfc79550413998b2fb99
ncb6206 <ncb6206@gmail.com>
2023-01-31 17:03:02 +0900

    10x19 클론코딩

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,34 @@
-import React, { Fragment } from "react";
 import "./scss/styles.scss";
 
 function App() {
   return (
-    <Fragment>
-      <header>
-        <nav>
-          <ul>
-            <li>Nail Studios</li>
-            <li>Shop Polish</li>
-          </ul>
-          <ul>
-            <li>Journal</li>
-          </ul>
-        </nav>
-      </header>
-      <div className="hero">
-        <h4>The Uptown Collection</h4>
-        <h3>
-          Meet our newest nails, designed in collaboration with our friends on the Upper East Side.
-        </h3>
-        <a href="#">Read More</a>
+    <body>
+      <div className="main">
+        <span></span>
+        <span></span>
+        <span className="menu">Menu</span>
+        <div className="number__row">
+          <div className="number">#10</div>
+          <div className="number">#9</div>
+          <div className="number">#8</div>
+          <div className="number">#7</div>
+          <div className="number">#6</div>
+        </div>
+        <div className="center-image"></div>
+        <div className="number__row">
+          <div className="number">#5</div>
+          <div className="number">#4</div>
+          <div className="number">#3</div>
+          <div className="number">#2</div>
+          <div className="number">#1</div>
+        </div>
+        <span></span>
+        <div className="scrolling__text">
+          <span>This is a very long text that should scroll using CSS3 Animations and not JS</span>
+        </div>
+        <span></span>
       </div>
-      <section className="under-hero">
-        <img src="https://source.unsplash.com/random/820x550/?programming" />
-        <div className="under-hero__content">
-          <div className="wrapper">
-            <h4>The Studio</h4>
-            <h3>
-              Book a manicure at our Soho flagship studio or our new Uptown studio, 20 East 69th
-              Street at Madison Avenue.
-            </h3>
-            <a href="#">Book a Manicure</a>
-          </div>
-        </div>
-      </section>
-      <section className="blog">
-        <article className="blog__post">
-          <img src="https://source.unsplash.com/random/500x500/?programming" />
-          <div className="post__content">
-            <span className="post__date">Feb 25, 2020</span>
-            <h4>New and Now: The Uptown Collection</h4>
-            <a href="#">Read Story</a>
-          </div>
-        </article>
-        <article className="blog__post">
-          <img src="https://images.unsplash.com/photo-1583255448430-17c5eda08e5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
-          <div className="post__content">
-            <span className="post__date">Feb 25, 2020</span>
-            <h4>New and Now: The Uptown Collection</h4>
-            <a href="#">Read Story</a>
-          </div>
-        </article>
-        <article className="blog__post">
-          <img src="https://images.unsplash.com/photo-1583307266943-e0055bb40037?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1421&q=80" />
-          <div className="post__content">
-            <span className="post__date">Feb 25, 2020</span>
-            <h4>New and Now: The Uptown Collection</h4>
-            <a href="#">Read Story</a>
-          </div>
-        </article>
-      </section>
-      <section className="gallery">
-        <div className="gallery__poster">
-          <img src="https://source.unsplash.com/random/500x500/?programming" />
-        </div>
-        <div className="gallery__poster">
-          <img src="https://source.unsplash.com/random/500x500/?programming" />
-        </div>
-        <div className="gallery__poster">
-          <img src="https://source.unsplash.com/random/500x500/?programming" />
-        </div>
-        <div className="gallery__poster">
-          <img src="https://source.unsplash.com/random/500x500/?programming" />
-        </div>
-        <div className="gallery__poster">
-          <img src="https://source.unsplash.com/random/500x500/?programming" />
-        </div>
-        <div className="gallery__poster">
-          <img src="https://source.unsplash.com/random/500x500/?programming" />
-        </div>
-        <div className="gallery__poster">
-          <img src="https://source.unsplash.com/random/500x500/?programming" />
-        </div>
-        <div className="gallery__poster">
-          <img src="https://source.unsplash.com/random/500x500/?programming" />
-        </div>
-      </section>
-      <footer className="footer">
-        <div className="footer__column">
-          <span className="column__title">Support</span>
-          <ul>
-            <li>F.A.Q.</li>
-            <li>Privacy Policu</li>
-            <li>Terms and Conditions</li>
-            <li>Accessibility</li>
-          </ul>
-        </div>
-        <div className="footer__column">
-          <span className="column__title">Follow Us</span>
-          <ul>
-            <li>Instagram</li>
-            <li>Twitter</li>
-          </ul>
-        </div>
-        <div className="footer__column">
-          <span className="column__title">Support</span>
-          <ul>
-            <li>F.A.Q.</li>
-            <li>Privacy Policu</li>
-            <li>Terms and Conditions</li>
-            <li>Accessibility</li>
-          </ul>
-        </div>
-      </footer>
-    </Fragment>
+    </body>
   );
 }
 
