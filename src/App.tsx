3cf4b684438a130d3a36633b004a8670dd8f33b0
ncb6206 <ncb6206@gmail.com>
2023-02-01 00:04:09 +0900

    Tolv 클론코딩

--- src/App.tsx
+++ src/App.tsx
@@ -3,140 +3,58 @@ import "./scss/styles.scss";
 function App() {
   return (
     <body>
+      <header>
+        <nav>
+          <ul>
+            <li>Products</li>
+            <li>Stockists</li>
+            <li>About us</li>
+          </ul>
+        </nav>
+        <h1 className="logo">
+          T<div>o</div>
+          lv
+        </h1>
+        <span>🔍</span>
+      </header>
       <main>
         <div className="hero">
-          <div className="hero__photo"></div>
-          <h2>News that lasts.</h2>
+          <div className="hero__img"></div>
+          <div className="hero__text">
+            <h2>Lift the blind to let in the light. A moment of calm before the day begins.</h2>
+            <span>
+              Featuring <a href="#">Cherry sofa</a> and
+              <a href="#">Kile coffee table</a>
+            </span>
+          </div>
+        </div>
+        <div className="banner">
+          <h3 className="banner__title">Time for Living</h3>
           <p>
-            <span>About</span> Schwartz Media publishes intelligent news and current affairs that
-            breaks the 24-hour news cycle. We offer a nuanced examination of Australia and the
-            world, focused on fresh insight and literary expression. Our audience reads to know, not
-            just to agree. We invest in long-form journalism where the issues demand it, providing
-            writing of a quality that makes difficult topics clear. Schwartz Media publishes
-            Australia’s most respected writers across The Saturday Paper, The Monthly magazine and
-            the daily podcast 7am, alongside our sister publications, Quarterly Essay and Australian
-            Foreign Affairs.
+            Take time for life’s little moments. Browsing the news as you eat breakfast. Setting the
+            table for hungry guests. Sinking in to your favourite armchair. At Tolv, your daily
+            rituals are at the heart of our design. We make furniture to give you your best day,
+            every day.
           </p>
+          <span>
+            Find out more <a href="#">About us</a>
+          </span>
         </div>
-        <section className="products">
-          <article className="product">
-            <div>
-              <h4 className="product__category">Journalism</h4>
-              <p className="product__description">
-                Our journalists create in-depth, independent, original public interest reporting,
-                focusing on storytelling and insight.
-              </p>
-              <a href="#" className="btn">
-                Learn more
-              </a>
-            </div>
-            <img src="https://source.unsplash.com/random/500x360" />
-          </article>
-          <article className="product">
-            <div>
-              <h4 className="product__category">Journalism</h4>
-              <p className="product__description">
-                Our journalists create in-depth, independent, original public interest reporting,
-                focusing on storytelling and insight.
-              </p>
-              <a href="#" className="btn">
-                Learn more
-              </a>
-              <a href="#" className="btn">
-                Learn more
-              </a>
-            </div>
-            <img src="https://source.unsplash.com/random/500x360" />
-          </article>
-          <article className="product">
-            <div>
-              <h4 className="product__category">Journalism</h4>
-              <p className="product__description">
-                Our journalists create in-depth, independent, original public interest reporting,
-                focusing on storytelling and insight.
-              </p>
-              <a href="#" className="btn">
-                Learn more
-              </a>
-            </div>
-            <img src="https://source.unsplash.com/random/500x360" />
-          </article>
-          <article className="product">
-            <div>
-              <h4 className="product__category">Journalism</h4>
-              <p className="product__description">
-                Our journalists create in-depth, independent, original public interest reporting,
-                focusing on storytelling and insight.
-              </p>
-              <a href="#" className="btn">
-                Learn more
-              </a>
-            </div>
-            <img src="https://source.unsplash.com/random/500x360" />
-          </article>
-          <div className="line">
-            <article className="product">
-              <div>
-                <h4 className="product__category">Journalism</h4>
-                <img src="https://source.unsplash.com/random/500x360" />
-                <p className="product__description">
-                  Our journalists create in-depth, independent, original public interest reporting,
-                  focusing on storytelling and insight.
-                </p>
-                <a href="#" className="btn">
-                  Learn more
-                </a>
-              </div>
-            </article>
-            <article className="product">
-              <div>
-                <h4 className="product__category">Journalism</h4>
-                <img src="https://source.unsplash.com/random/500x360" />
-                <p className="product__description">
-                  Our journalists create in-depth, independent, original public interest reporting,
-                  focusing on storytelling and insight.
-                </p>
-                <a href="#" className="btn">
-                  Learn more
-                </a>
-              </div>
-            </article>
-          </div>
+        <section className="gallery">
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
+          <div className="gallery__item"></div>
         </section>
-        <div className="links">
-          <div className="link">
-            <h4>Careers</h4>
-            <span>Work at the country’s leading independent publisher. </span>
-            <a href="#" className="btn">
-              Learn more
-            </a>
-          </div>
-          <div className="link">
-            <h4>Careers</h4>
-            <span>Work at the country’s leading independent publisher. </span>
-            <a href="#" className="btn">
-              Learn more
-            </a>
-          </div>
-          <div className="link">
-            <h4>Careers</h4>
-            <span>Work at the country’s leading independent publisher. </span>
-            <a href="#" className="btn">
-              Learn more
-            </a>
-          </div>
-        </div>
       </main>
-      <footer>
-        <div className="footer__top">
-          <span>Schwartz</span>
-          <ul>
-            <li>Schwartz Media</li>
-            <li>Schwartz Media</li>
-            <li>Schwartz Media</li>
-          </ul>
-        </div>
-      </footer>
     </body>
   );
 }
