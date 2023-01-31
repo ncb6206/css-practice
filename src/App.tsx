e69cf460bda6a020449b2646466d37a2401eb8c2
ncb6206 <ncb6206@gmail.com>
2023-02-01 01:12:38 +0900

    Beige 클론코딩

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,235 @@ import "./scss/styles.scss";
 function App() {
   return (
     <body>
-      <h1>Rodic Davidson</h1>
-      <main>
+      <header className="header">
+        <h1>Beige</h1>
         <nav>
           <ul>
-            <li>Projects</li>
-            <li>Practice</li>
-            <li>News</li>
-            <li>Exhibition</li>
-            <li>Contact</li>
+            <li>
+              <span></span>
+              <a href="#">Kultur</a>
+            </li>
+            <li>
+              <span></span>
+              <a href="#">Menschen</a>
+            </li>
+            <li>
+              <span></span>
+              <a href="#">Reisen</a>
+            </li>
+            <li>
+              <span></span>
+              <a href="#">Design</a>
+            </li>
+            <li>
+              <span></span>
+              <a href="#">Mode</a>
+            </li>
+            <li>
+              <span></span>
+              <a href="#">Pflege</a>
+            </li>
+            <li>
+              <span></span>
+              <a href="#">Fair</a>
+            </li>
           </ul>
         </nav>
-        <div className="photo">
-          <img src="https://source.unsplash.com/random/1" />
-          <h4>Suffolk Farmstead</h4>
-        </div>
-        <div className="photo">
-          <img src="https://source.unsplash.com/random/2" />
-          <h4>Suffolk Farmstead</h4>
-        </div>
-        <div className="photo">
-          <img src="https://source.unsplash.com/random/3" />
-          <h4>Suffolk Farmstead</h4>
-        </div>
-        <div className="photo">
-          <img src="https://source.unsplash.com/random/4" />
-          <h4>Suffolk Farmstead</h4>
-        </div>
-        <div className="photo">
-          <img src="https://source.unsplash.com/random/5" />
-          <h4>Suffolk Farmstead</h4>
-        </div>
-        <div className="photo">
-          <img src="https://source.unsplash.com/random/6" />
-          <h4>Suffolk Farmstead</h4>
-        </div>
-        <div className="photo">
-          <img src="https://source.unsplash.com/random/7" />
-          <h4>Suffolk Farmstead</h4>
-        </div>
+        <span>🔍</span>
+      </header>
+      <main className="posts">
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
+        <section className="post">
+          <header className="post__header">
+            <span className="post__date">24. Februar 2020</span>
+            <span className="post__author">Marie Jaster</span>
+          </header>
+          <div className="post__content">
+            <div className="visible">
+              <h3>Hol dir Farbe in deine Inbox</h3>
+            </div>
+            <div className="invisible"></div>
+          </div>
+          <footer>
+            <span className="category">Newsletter</span>
+            <span className="comments">1 Kommentar</span>
+          </footer>
+        </section>
       </main>
     </body>
   );
